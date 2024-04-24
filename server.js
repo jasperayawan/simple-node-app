164553e000b5bf98f62a69c9052e908e051e4dc4
Jasper Ayawan <ejayawan22@gmail.com>
2024-04-24 15:29:47 +0800

    added a parse server

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 const express = require("express");
-
+const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
 const app = express();
 const port = 3000;
+
+
+const api = new ParseServer({
+    databaseURI: "mongodb+srv://ejayawan22:kuKvYG2VfIEss9Fj@cluster0.yjbhaod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    appId: "123",
+    masterKey: "1234",
+    serverURL: `http://localhost:3000/parse`,
+    appName: "simple-node-app",
+})
+
+const dashboardConfig = new ParseDashboard({
+    apps: [
+        {
+            appId: "123",
+            masterKey: "1234",
+            serverURL: `http://localhost:3000/parse`,
+            appName: "simple-node-app"
+        }
+    ]
+})
+
+api.start();
+
+app.use('/parse', api.app)
+app.use('/dashboard', dashboardConfig)
+
+
 
 app.get('/api', (req, res) => {
     res.send({
