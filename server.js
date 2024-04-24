32b069f3c8df167671677f4cb04d94d987ea0389
Jasper Ayawan <ejayawan22@gmail.com>
2024-04-24 16:48:01 +0800

    update parse

--- server.js
+++ server.js
@@ -6,49 +6,44 @@ const fs = require("fs");
 const https = require('https')
 
 const options = {
-    key: fs.readFileSync(path.join(__dirname, 'private.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'certificate.crt')),
-  };
+  key: fs.readFileSync(path.join(__dirname, 'private.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'certificate.crt')),
+};
 
 const app = express();
 const port = 3000;
 
-
 const api = new ParseServer({
-    databaseURI: "mongodb+srv://ejayawan22:kuKvYG2VfIEss9Fj@cluster0.yjbhaod.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    appId: "123",
-    masterKey: "1234",
-    serverURL: `http://localhost:3000/parse`,
-    appName: "simple-node-app",
-})
+  databaseURI: "mongodb+srv://ejayawan22:kuKvYG2VfIEss9Fj@cluster0.yjbhaod.mongodb.net/?retryWrites=true&w=majority",
+  appId: "123",
+  masterKey: "1234",
+  serverURL: `http://localhost:3000/parse`,
+  appName: "simple-node-app"
+});
 
-const dashboardConfig = new ParseDashboard({
-    apps: [
-        {
-            appId: "123",
-            masterKey: "1234",
-            serverURL: `http://localhost:3000/parse`,
-            appName: "simple-node-app"
-        }
-    ]
-})
+const dashboardConfig = {
+  apps: [
+    {
+      appId: "123",
+      masterKey: "1234",
+      serverURL: `http://localhost:3000/parse`,
+      appName: "simple-node-app"
+    }
+  ]
+};
 
-api.start();
+app.use('/parse', new ParseServer(api));
 
-app.use('/parse', api.app)
-app.use('/dashboard', dashboardConfig)
-
-
+app.use('/dashboard', new ParseDashboard(dashboardConfig));
 
 app.get('/api', (req, res) => {
-    res.send({
-        name: 'jasper'
-    })
-})
+  res.send({
+    name: 'jasper'
+  });
+});
 
 app.listen(port, () => {
-    console.log("server listening to port:", port)
-})
+  console.log("server listening to port:", port);
+});
 
-const server = https.createServer(options, app)
-server.listen(443, () => console.log('Server running on port 443'))
+https.createServer(options, app).listen(443, () => console.log('Server running on port 443'));
